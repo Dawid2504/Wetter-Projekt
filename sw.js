13ac21043a4f7bc60811bb210e1bb1a56c141836
Dawid2504 <144803695+Dawid2504@users.noreply.github.com>
2026-06-03 13:17:24 +0000

    PWA Support hinzufügen

---
+++ sw.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = "weltinfos-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",
+];
+
+// Installieren
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        console.log("Cache geöffnet");
+        return cache.addAll(urlsToCache);
+      })
+      .catch((err) => {
+        console.log("Cache-Fehler:", err);
+      }),
+  );
+});
+
+// Fetch-Strategie: Network first, then cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response.status === 200) {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseClone);
+          });
+        }
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      }),
+  );
+});
+
+// Update: Alten Cache löschen
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log("Lösche alten Cache:", cacheName);
+            return caches.delete(cacheName);
+          }
+        }),
+      );
+    }),
+  );
+});
